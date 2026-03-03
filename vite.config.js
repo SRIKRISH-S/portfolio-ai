2d6779b2122aedebe7b63920a56dd934d723a46b
SRIKRISH-S <krishn9706@gmail.com>
2026-03-03 23:03:14 +0530

    Configure GH Pages deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/portfolio-ai/' // Used for GitHub Pages serving, the repo name
 })
